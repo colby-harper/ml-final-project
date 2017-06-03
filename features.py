04f4a8b86fe4e2f5a142dba7e4b291b9cdd63890
Colby Harper <charper@scu.edu>
2017-06-02 20:40:19 -0700

    added adj_close in dataSet.py to add output to features.py

--- features.py
+++ features.py
@@ -18,14 +18,14 @@ if __name__ == "__main__":
 	#print (df)
 	data_array = np.array(df)
 	#array for new features
-	features = np.zeros((data_array.shape[0],5))
+	features = np.zeros((data_array.shape[0],6))
 
 	j = 0
-	for i in range(0,data_array.shape[0]):
+	for i in range(0,(data_array.shape[0]-2)):
 		#Check if in a new ticker group (skip first element if so)
-		if (data_array[i][4] != j):
-			j = data_array[i][4]
-			i += 1
+		if (data_array[i+2][5] != j):
+			j = data_array[i+2][5]
+			i += 3
 		#Calculate price percent difference - 1st column
 		prev_price = data_array[i-1][0]
 		curr_price = data_array[i][0]
@@ -34,24 +34,37 @@ if __name__ == "__main__":
 		
 		#Calculate volume percent difference - 2nd column
 		prev_vol = data_array[i-1][1]
-		#if (prev_vol == 0):
-		#	features[i][1] = 1000
-		#else:
 		vol_diff = data_array[i][1] - prev_vol
 		features[i][1] = (vol_diff)/(prev_vol)
 
-		#Calculate percent change from high
+		#Calculate percent change from high - 3rd column
 		prev_high = data_array[i][2]
 		price_diff_high = curr_price - prev_high
 		features[i][2] = (price_diff_high)/prev_high
 
-		#Calculate percent change from low
+		#Calculate percent change from low - 4th column
 		prev_low = data_array[i][3]
 		price_diff_low = curr_price - prev_low
 		features[i][3] = (price_diff_low)/prev_low
 
-		#Calculate volatility
+		#Calculate volatility - 5th column
 		vola_diff = prev_high - prev_low
 		features[i][4] = (vola_diff)/curr_price
 
+		#Calculate the future percent change - 6th column
+		future_price = data_array[i+2][4]
+		trade_price = data_array[i+1][4]
+		percent_change = ((future_price - trade_price)/trade_price)*100
+		stock_category = None
+		if percent_change > 5:
+			stock_category = 0
+		elif percent_change < 5:
+			stock_category = 1
+		else:
+			stock_category = 2
+		features[i][5] = stock_category
+	#get rid of zeros
+	features = np.delete(features, 0, axis=0)
+	features = np.delete(features, -1, axis=0)
+	features = np.delete(features, -1, axis=0)
 	print (features)
