155e8954e06bce557a33b2c8d212edd671682247
Colby Harper <charper@scu.edu>
2017-06-04 17:19:17 -0700

    only use stocks with full data

--- dataSet.py
+++ dataSet.py
@@ -23,11 +23,30 @@ def getStockPrices():
 	ticker_id = np.array([0])
 	name = ''
 	j = 0
+	i = 0
 	remove = set()
+	ticker_count = 0
+	max_ticker_count = 0
+	max_tick_index = set()
 	for index, row in feature_data.iterrows():	
 		if row.ticker != name:
+			ticker_count = i
+			if ticker_count > max_ticker_count:
+				for _ in range(len(max_tick_index)):
+					remove.add(max_tick_index.pop())
+				#remove.add(max_tick_index)
+				max_ticker_count = ticker_count
+				max_tick_index.add(j)
+			elif ticker_count < max_ticker_count:
+				remove.add(j)
+			else:
+				max_tick_index.add(j)
+
 			name = row.ticker
 			j += 1
+			i = 0
+		
+
 		adj_volume = row.adj_volume
 		#If the volume is zero, add the ticker id to the remove set
 		if (adj_volume == 0):	
@@ -40,9 +59,11 @@ def getStockPrices():
 		newRow = np.array([adj_close, adj_volume, adj_high, adj_low, adj_open])
 		data_array = np.vstack((data_array, newRow))
 		ticker_id = np.vstack((ticker_id, j))
-			
+		i +=1
+
 	ticker_id = ticker_id.astype(np.float64)
 	data_array = np.append(data_array, ticker_id, 1)
+
 	#loop through the array.  If a row's ticker id is in the set, delete that row.
 	i = 0
 	while i < len(data_array):
